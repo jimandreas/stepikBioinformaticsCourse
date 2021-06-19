5f7d57cf2c21faf82e97d35887f307cd243c2421
Jim Andreas <jim.r.andreas@gmail.com>
2021-06-19 17:40:01 +0200

    part way through 1.8

---
+++ src/main/kotlin/util/HammingDistance.kt
@@ -0,0 +1,13 @@
+package util
+
+fun hammingDistance(str1: String, str2: String): Int {
+
+    var hammingCount = 0
+    str1.filterIndexed { index, char ->
+        if (str2[index] != char) {
+            hammingCount += 1
+        }
+        true
+    }
+    return hammingCount
+}
