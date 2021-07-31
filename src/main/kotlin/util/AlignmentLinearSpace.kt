6107df1add895a59d74071216a2a96ca819b28ea
Jim Andreas <jim.r.andreas@gmail.com>
2021-07-31 12:32:21 +0200

    in progress S05c13p14 LinearSpaceAlignment

--- src/main/kotlin/util/AlignmentLinearSpace.kt
+++ src/main/kotlin/util/AlignmentLinearSpace.kt
@@ -1,12 +1,10 @@
 @file:Suppress(
     "UnnecessaryVariable", "unused", "MemberVisibilityCanBePrivate", "LiftReturnOrAssignment",
-    "IntroduceWhenSubject", "VARIABLE_WITH_REDUNDANT_INITIALIZER", "UNUSED_PARAMETER", "UNUSED_VARIABLE"
+    "IntroduceWhenSubject", "VARIABLE_WITH_REDUNDANT_INITIALIZER", "UNUSED_PARAMETER", "UNUSED_VARIABLE",
+    "UNUSED_CHANGED_VALUE"
 )
 
 package util
-
-import ResourceReader
-import kotlin.math.max
 
 /**
  * Code Challenge: Implement LinearSpaceAlignment to solve the Global Alignment Problem for a large dataset.
@@ -35,13 +33,138 @@ class AlignmentLinearSpace(
 ) {
 
 
+    val alsme = AlignmentLinearSpaceMiddleEdge(mMatchValue, uMismatchValue, sigmaGapPenalty, useBLOSUM62)
+
     /**
      * Alignment using linear space (plus a bit of recursion!!)
      *
-    * @return Triple of score, alignedRow, alignedCol
-    */
+     * @return Triple of score, alignedRow, alignedCol
+     */
     fun alignmentLinearSpace(sRow: String, tCol: String): Triple<Int, String, String> {
-      return Triple(0, "", "")
+        val sRowOut = StringBuilder()
+        val tColOut = StringBuilder()
+
+        val output = linearSpaceAlignmentRecursive(
+            sRow, tCol,
+            0, tCol.length,
+            0, sRow.length,
+            sRowOut, tColOut
+        )
+        return Triple(0, sRowOut.toString(), tColOut.toString())
+    }
+
+    /*
+     * link to:  http://rosalind.info/problems/ba5l/
+     */
+    val pseudoCode = """
+        
+LinearSpaceAlignment(v, w, top, bottom, left, right)
+  if left = right
+    output path formed by bottom − top vertical edges
+  if top = bottom
+    output path formed by right − left horizontal edges
+  middle ← ⌊ (left + right)/2⌋
+  midEdge ← MiddleEdge(v, w, top, bottom, left, right)
+  midNode ← vertical coordinate of the initial node of midEdge
+  LinearSpaceAlignment(v, w, top, midNode, left, middle)
+  output midEdge
+  if midEdge = "→" or midEdge = "↘"
+    middle ← middle + 1
+  if midEdge = "↓" or midEdge ="↘"
+    midNode ← midNode + 1
+  LinearSpaceAlignment(v, w, midNode, bottom, middle, right)
+         
+         """.trimIndent()
+
+    fun linearSpaceAlignmentRecursive(
+        sRow: String, tCol: String,
+        top: Int, bottom: Int,
+        left: Int, right: Int,
+        sRowOut: StringBuilder, tColOut: StringBuilder
+    ): Triple<Int, StringBuilder, StringBuilder> {
+
+        if (left == right) {
+            return Triple(0, sRowOut, tColOut)
+        }
+        if (top == bottom) {
+            return Triple(0, sRowOut, tColOut)
+        }
+
+        var middle = (left + right) / 2
+        val middleEdge = alsme.findMiddleEdge(sRow, tCol)
+        var midNodeColIndex = middleEdge.first.first
+        val midNodeRowIndex = middleEdge.first.second
+
+        // work up new substrings for top left quadrant
+        val sRowTopLeft = sRow.substring(0, midNodeRowIndex)
+        val tColTopLeft = tCol.substring(0, midNodeColIndex)
+
+        println("'$sRowTopLeft' '$tColTopLeft' TOP LEFT")
+
+        val topLeft = linearSpaceAlignmentRecursive(
+            sRowTopLeft, tColTopLeft,
+            0, midNodeColIndex,
+            0, midNodeRowIndex,
+            sRowOut, tColOut
+        )
+
+        val dir = middleEdgeDir(middleEdge)
+        if (dir == 'R' || dir == 'M') {
+            middle++
+        }
+        if (dir == 'D' || dir == 'M') {
+            midNodeColIndex++
+        }
+
+        // work up new substrings for top left quadrant
+        val sRowBottomRight = sRow.substring(middle, sRow.length)
+        val tColBottomRight = tCol.substring(midNodeColIndex, tCol.length)
+
+        println("'$sRowBottomRight' '$tColBottomRight' BOTTOM RIGHT")
+
+        val bottomRight = linearSpaceAlignmentRecursive(
+            sRowBottomRight, tColBottomRight,
+            middle, sRow.length,
+            midNodeColIndex, tCol.length,
+            sRowOut, tColOut
+        )
+
+        when (dir) {
+            'R' -> {
+                sRowOut.append(sRow[middle-1])
+                tColOut.append("-")
+            }
+            'D' -> {
+                sRowOut.append("-")
+                tColOut.append(tCol[midNodeColIndex])
+            }
+            'M' -> {
+                sRowOut.append(sRow[middle-1])
+                tColOut.append(tCol[midNodeColIndex-1])
+            }
+        }
+
+        return Triple(0, sRowOut, tColOut)
+    }
+
+    /**
+     *  reverse engineer the direction of the edge
+     *  given the coordinate pair of the edge.
+     *
+     *  @return R for row="-', D for column="-", and M for match/mismatch (down and to the right)
+     */
+    fun middleEdgeDir(middleEdge: Pair<Pair<Int, Int>, Pair<Int, Int>>): Char {
+        val r1 = middleEdge.first.first
+        val c1 = middleEdge.first.second
+        val r2 = middleEdge.second.first
+        val c2 = middleEdge.second.second
+
+        when {
+            r1 == r2 && c1 <= c2 -> return 'R'
+            r1 <= r2 && c1 == c2 -> return 'D'
+            r1 < r2 && c1 < c2 -> return 'M'
+        }
+        return '?'
     }
 
 
