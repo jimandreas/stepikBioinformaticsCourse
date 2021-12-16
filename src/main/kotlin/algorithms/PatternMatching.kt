785324e63c8382231235d888e3ab94dfe204a270
Jim Andreas <jim.r.andreas@gmail.com>
2021-12-16 15:11:57 +0100

    Pattern Matching ch9 - Construct a Trie - completed

--- src/main/kotlin/algorithms/PatternMatching.kt
+++ src/main/kotlin/algorithms/PatternMatching.kt
@@ -38,5 +38,60 @@ class PatternMatching {
      * must be the symbol labeling the edge.
      */
 
+    fun trieConstruction(patterns: List<String>): Map<Int, Map<Char, Int>> {
+
+        // for each node - the list of characters at that node
+        val trieDictionary: MutableMap<Int, MutableList<Char>> = mutableMapOf()
+
+        // for each node - what node number the characters map to
+        val trieMap: MutableMap<Int, MutableMap<Char, Int>> = mutableMapOf()
+
+        var nextNodeNum = 1
+        for (p in patterns) {
+            var currentNode = 0
+            outerloop@ for (idx in 0 until p.length) {
+
+                var c = p[idx]
+                val clist = trieDictionary[currentNode]
+                if (clist == null || !clist.contains(c)) {
+
+                    // if this is the first character, make a connection
+                    //   from the root node (0) to the first
+                    //   character of this string
+                    if (idx == 0) {
+                        if (trieMap.containsKey(0)) {
+                            trieMap[0]!![c] = nextNodeNum
+                        } else {
+                            trieMap[0] = mutableMapOf(Pair(c, nextNodeNum))
+                        }
+                        trieDictionary.addTo(0, c)
+                    } else {
+                        // the character is not in the dictionary at this level.
+                        //   Add the character to the dictionary for this node, and then
+                        //   work through the rest of the string as a new set of nodes
+                        trieDictionary.addTo(currentNode, c)
+                        trieMap[currentNode]!![c] = nextNodeNum
+                    }
+
+                    // now create new nodes for all the remaining letters in the string
+
+                    for (idx2 in idx + 1 until p.length) {
+                        c = p[idx2]
+                        trieDictionary.addTo(nextNodeNum, c)
+                        trieMap[nextNodeNum] = mutableMapOf(Pair(c, nextNodeNum+1))
+                        nextNodeNum++
+                    }
+                    nextNodeNum++
+                    break@outerloop
+                } else {
+                    // found the character in the dictionary.
+                    //   look up the next index
+                    currentNode = trieMap[currentNode]!![c]!!
+                }
+            }
+        }
+        return trieMap
+    }
+
 
 }
--- src/test/kotlin/S09C03P04TrieConstructionTest.kt
+++ src/test/kotlin/S09C03P04TrieConstructionTest.kt
@@ -3,21 +3,23 @@
     "ReplaceManualRangeWithIndicesCalls", "ReplaceSizeZeroCheckWithIsEmpty", "SameParameterValue"
 )
 
-import algorithms.*
+import algorithms.PatternMatching
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertContentEquals
 
 /**
  * See also:
- * Stepik: no equivalent problem
- * Rosalind: https://rosalind.info/problems/ba8d/
+ * Stepik: https://stepik.org/lesson/240376/step/4?unit=212722
+ * Rosalind: https://rosalind.info/problems/ba9a/
  *
- * Youtube:
- * Soft k-Means Clustering 8/9
- * https://www.youtube.com/watch?v=fpM0iZTjLhM
+ * Using the Trie 2/10
+ * https://www.youtube.com/watch?v=9U0ynguwoNA
+ *
+ * From a Trie to a Suffix Tree (3/10)
+ * https://www.youtube.com/watch?v=LB-ANFydv30
  */
 
 internal class S09C03P04TrieConstructionTest {
@@ -50,11 +52,57 @@ internal class S09C03P04TrieConstructionTest {
      */
     @Test
     @DisplayName("Pattern Matching Trie Construction Problem")
-    fun clustersHierarchicalSampleTest() {
+    fun patternMatchingTrieConstructionSampleTest() {
 
-        val inputString = "ATAGA ATC GAT"
+        val inputString = "ATAGA ATC GAT".split(" ")
+
+        val result = pm.trieConstruction(inputString)
+
+        val expectedResult = """
+            0 1 A
+            1 2 T
+            2 3 A
+            3 4 G
+            4 5 A
+            2 6 C
+            0 7 G
+            7 8 A
+            8 9 T
+        """.trimIndent().lines().toMutableList().sorted()
+
+        val parseResult = parseResult(result)
+
+        //println(parseResult.joinToString("\n"))
+
+        assertContentEquals(expectedResult, parseResult)
 
 
+    }
+
+    fun parseResultRosalindFormatting(result: Map<Int, Map<Char, Int>>): List<String> {
+        val outList: MutableList<String> = mutableListOf()
+        val str = StringBuilder()
+        for (k in result.keys.sorted()) {
+            for (c in result[k]!!.keys) {
+                str.append("$k->${result[k]!![c]}:$c")
+                outList.add(str.toString())
+                str.clear()
+            }
+        }
+        return outList
+    }
+
+    fun parseResult(result: Map<Int, Map<Char, Int>>): List<String> {
+        val outList: MutableList<String> = mutableListOf()
+        val str = StringBuilder()
+        for (k in result.keys.sorted()) {
+            for (c in result[k]!!.keys) {
+                str.append("$k ${result[k]!![c]} $c")
+                outList.add(str.toString())
+                str.clear()
+            }
+        }
+        return outList
     }
 
 }
