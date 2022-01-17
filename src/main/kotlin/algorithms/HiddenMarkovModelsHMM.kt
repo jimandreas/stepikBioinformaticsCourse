bcbc62dfb7e29c74e493926dd8f9bee2880bdc84
Jim Andreas <jim.r.andreas@gmail.com>
2022-01-17 15:08:10 +0100

    Hidden Markov Models - Probability of an Outcome Given a Hidden Path Problem- completed.

--- src/main/kotlin/algorithms/HiddenMarkovModelsHMM.kt
+++ src/main/kotlin/algorithms/HiddenMarkovModelsHMM.kt
@@ -19,6 +19,11 @@ class HiddenMarkovModelsHMM {
      * The Decoding Problem (4/10)
      * https://www.youtube.com/watch?v=Xr0zDtbeLik&list=PLQ-85lQlPqFPnk31Uut2ajVkBvlFmMtdx&index=4
      *
+     * The Viterbi Algorithm (5/10)
+     * https://www.youtube.com/watch?v=0dVUfYF8ko0&list=PLQ-85lQlPqFPnk31Uut2ajVkBvlFmMtdx&index=5
+     *
+     * Wikipedia:
+     * https://en.wikipedia.org/wiki/Viterbi_algorithm
      */
 
     fun runMarkovModel(pathPi: String, stateCount: Int, matrix: D2Array<Double>): Double {
@@ -34,6 +39,46 @@ class HiddenMarkovModelsHMM {
             prob *= nextProb
 
             currentState = newState
+        }
+
+        return prob
+    }
+
+    /**
+
+     Given: A string [emissionStringx],
+     followed by the alphabet Σ from which x was constructed,
+     followed by a hidden path π [pathPi], followed by the
+     states States and emission [matrix] Emission of an HMM (Σ, States, Transition, Emission).
+
+     Return: The conditional probability Pr(x|π) that string x will be emitted by the HMM given the hidden path π.
+     */
+    fun calculateEmissionProbabilty(
+        emissionStringx: String,
+        emissionCharList: List<String>,
+        pathPi: String,
+        pathCharList: List<String>,
+        matrix: D2Array<Double>
+    ): Double {
+
+        // there is no initial prob -
+        // calculate only probs of emission characters
+        //var prob = 1.0 / stateCount.toDouble()
+
+
+        val baseState = 'A'
+        val baseEmission = 'x'
+
+        var prob = 1.0
+
+        for (i in 0 until pathPi.length) {
+
+            val curStateIndex = pathPi[i] - baseState
+            val emissionCharIndex = emissionStringx[i] - baseEmission
+
+            val emissionProb = matrix[curStateIndex, emissionCharIndex]
+
+            prob *= emissionProb
         }
 
         return prob
